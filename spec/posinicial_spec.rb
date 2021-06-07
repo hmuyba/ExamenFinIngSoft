1a6fd8300c6213999308d001dd6cb71cb7dbd4f7
Harold Muyba <hmuyba@gmail.com>
2021-06-07 18:08:56 -0400

    agregando segunda prueba tdd de ingresar 3,2 y devolver [3,2]

--- spec/posinicial_spec.rb
+++ spec/posinicial_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe "separar correctamente los caracteres introducidos en un string" 
         resultado=getInitialPosition(numero)
         expect(resultado).to eq([1,2])
     end
+
+    it "deberia devolver el array [3,2] al introducir el texto 3,2" do
+        numero = "3,2"
+        resultado = getInitialPosition(numero)
+        expect(resultado).to eq([3,2])
+    end
   
 
 end
